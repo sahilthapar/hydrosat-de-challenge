e53d88c4b2b4f36a9aca61dd10ad0c661cb38f22
Sahil Thapar <sahilthapar>
2025-04-13 16:30:50 -0700

    Add public read acl to S3 buckets

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,8 +26,94 @@ resource "aws_s3_bucket" "input_bucket" {
   bucket = var.input_bucket_name
 }
 
+resource "aws_s3_bucket_ownership_controls" "input_bucket_ownership" {
+  bucket = aws_s3_bucket.input_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "input_bucket_access" {
+  bucket                  = aws_s3_bucket.input_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "input_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.input_bucket_ownership,
+    aws_s3_bucket_public_access_block.input_bucket_access,
+  ]
+
+  bucket = aws_s3_bucket.input_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "input_bucket_policy" {
+  depends_on = [aws_s3_bucket_public_access_block.input_bucket_access]
+
+  bucket = aws_s3_bucket.input_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::${var.input_bucket_name}/*"
+      },
+    ]
+  })
+}
+
 resource "aws_s3_bucket" "output_bucket" {
   bucket = var.output_bucket_name
+}
+
+resource "aws_s3_bucket_ownership_controls" "output_bucket_ownership" {
+  bucket = aws_s3_bucket.output_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "output_bucket_access" {
+  bucket                  = aws_s3_bucket.output_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "output_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.output_bucket_ownership,
+    aws_s3_bucket_public_access_block.output_bucket_access,
+  ]
+
+  bucket = aws_s3_bucket.output_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "output_bucket_policy" {
+  depends_on = [aws_s3_bucket_public_access_block.output_bucket_access]
+
+  bucket = aws_s3_bucket.output_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::${var.output_bucket_name}/*"
+      },
+    ]
+  })
 }
 
 # Upload sample data to the input bucket
